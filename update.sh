ee0435bf166321727f8d1ec33f38622451987c17
benteng <benteng@maihaoche.com>
2018-11-29 14:48:08 +0800

    增加更新脚本

--- update.sh
+++ update.sh
@@ -6,10 +6,10 @@ else
   if [ -z "$(git status --untracked-files=no --porcelain)" ]; then 
     echo "文件没有变化"
   else
-    npm run build
-    npm version patch
     git add .
     git commit -m "$1"
+    npm run build
+    npm version patch
     npm publish
   fi
 fi
