f73819ffeb55475dbd229a8621d536389659cfae
benteng <benteng@maihaoche.com>
2018-11-29 14:48:53 +0800

    更新脚本

--- sample/update.sh
+++ sample/update.sh
@@ -4,13 +4,13 @@ if [ -z "$1" ]; then
   echo "请输入message ./update.sh [message]"
 else
   sisyphus
-  if git diff-index --quiet HEAD --; then
-    echo "接口没有变化"
+  if [ -z "$(git status --untracked-files=no --porcelain)" ]; then 
+    echo "文件没有变化"
   else
-    npm run build
-    npm version patch
     git add .
     git commit -m "$1"
+    npm run build
+    npm version patch
     npm publish
   fi
 fi
